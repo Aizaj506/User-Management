cffd813e644a8a9944a72d64103030845cd2a420
Ahmed Ajaj <ahmed.ajaj@fyntune.com>
2025-10-15 19:08:16 +0530

    Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
+import Sidebar from "./components/sidbar/Sidebar"
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <Sidebar />
+    </>
   )
 }
 
---
+++ src/components/sidbar/Sidebar.tsx
@@ -0,0 +1,16 @@
+import { ChevronDownIcon } from "@heroicons/react/16/solid"
+
+const Sidebar = () => {
+  return (
+    <div className="w-64 h-screen bg-blue-900 text-white">
+      <div>
+        <section>
+            <h1 className="font-bold flex items-center">Super Admin <ChevronDownIcon className="size-5"/></h1>
+            
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
