92c5ec46192ea522a8c1dc5a8446503dba5ddec8
Ahmed Ajaj <ahmed.ajaj@fyntune.com>
2025-10-17 11:16:22 +0530

    Sidebar header

--- src/components/sidbar/Sidebar.tsx
+++ src/components/sidbar/Sidebar.tsx
@@ -1,12 +1,15 @@
-import { ChevronDownIcon } from "@heroicons/react/16/solid"
+import { ChevronDownIcon, Cog8ToothIcon } from "@heroicons/react/16/solid"
 
 const Sidebar = () => {
   return (
     <div className="w-64 h-screen bg-blue-900 text-white">
       <div>
-        <section>
-            <h1 className="font-bold flex items-center">Super Admin <ChevronDownIcon className="size-5"/></h1>
-            
+        <section className=" p-2">
+          <div className="flex justify-between">
+            <h1 className="font-bold flex items-center">Super Admin <ChevronDownIcon className="size-5 font-bold" /></h1>
+            <Cog8ToothIcon className="size-6" />
+          </div>
+          <p className="text-gray-400 font-semibold">admin.mail@company.com</p>
         </section>
       </div>
     </div>
